dc63a5bd78ece458f406fc6ecd099eab24b96f90
Peskov Sergey <peskovdev@proton.me>
2023-08-14 19:41:02 +0300

    Add main.go with todo list

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: router: chi, "chi render"
+
+	// TODO: run server
+}
