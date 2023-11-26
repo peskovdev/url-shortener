3245ea161476ccd722cd3f577b5db3f40715699f
Peskov Sergey <peskovdev@proton.me>
2023-11-26 23:26:13 +0300

    Add logging

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"github.com/peskovdev/url-shortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
 
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
