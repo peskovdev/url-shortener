7626e65115478b138ef1579b96cad9b36b7dacb2
Peskov Sergey <peskovdev@proton.me>
2023-11-27 18:03:56 +0300

    Implement storage methods

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,7 +3,8 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/mattn/go-sqlite3"
+	"github.com/peskovdev/url-shortener/internal/storage"
 )
 
 type Storage struct {
@@ -34,4 +35,48 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+
+	res, err := s.db.Exec("INSERT INTO url (alias, url) VALUES (?, ?)", alias, urlToSave)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return -1, fmt.Errorf("%s: %w", op, storage.ErrURLExist)
+		}
+		return -1, fmt.Errorf("%s: %w", op, err)
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return -1, fmt.Errorf("%s: %w", op, err)
+	}
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.sqlite.GetURL"
+	var resURL string
+
+	row := s.db.QueryRow("SELECT url FROM url WHERE alias=?", alias)
+	if err := row.Scan(&resURL); err != nil {
+		if err == sql.ErrNoRows {
+			return "", storage.ErrURLNotFound
+		}
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return resURL, nil
+}
+
+// TODO: implement delete handler
+func (s *Storage) DeleteURL(alias string) error {
+	const op = "storage.sqlite.DeleteURL"
+
+	_, err := s.db.Exec("DELETE FROM url WHERE alias=?", alias)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
